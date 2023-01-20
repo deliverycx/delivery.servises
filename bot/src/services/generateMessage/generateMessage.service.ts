115dd0f17902a0aeb11303da0b14832d12b0d3eb
xirurgx3d <xirurgx3d@gmail.com>
2023-01-20 15:27:34 +0300

    за столом

--- bot/src/services/generateMessage/generateMessage.service.ts
+++ bot/src/services/generateMessage/generateMessage.service.ts
@@ -10,7 +10,7 @@ export function generateMessage(body: CreateMessage.IRecivedBody): string {
 
     result += `\n\n`;
     result += `Данные о пользователе: \n`;
-    result += `Тип доставки: ${body.orderType} \n`;
+    result += `Тип доставки: ${body.orderType} ${body.orderType === 'ONSPOT' && '(За столом)'} \n`;
     result += `Адрес: ${ body.orderType === 'PICKUP' ? ' ' : body.address}\n`;
     result += `Телефон: ${body.phone}\n`;
     result += `Имя: ${body.name}\n`;
